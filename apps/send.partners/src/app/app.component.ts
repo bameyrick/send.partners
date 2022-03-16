4d1129959e793765f2ba3f288b85d4fcb1c8c070
Ben Meyrick <bameyrick@gmail.com>
2022-03-16 14:33:06 +0000

    initial commit

---
+++ apps/send.partners/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@send.partners/api-interfaces';
+
+@Component({
+  selector: 'send.partners-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
