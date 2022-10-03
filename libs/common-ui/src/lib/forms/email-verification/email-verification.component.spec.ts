eb6c6ec97fab8edc1e64b0d7211412302b1ee3fc
Ben Meyrick <bameyrick@gmail.com>
2022-10-03 14:00:00 +0100

    Email verification component tests

--- libs/common-ui/src/lib/forms/email-verification/email-verification.component.spec.ts
+++ libs/common-ui/src/lib/forms/email-verification/email-verification.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { delay } from '@qntm-code/utils';
+import { firstValueFrom } from 'rxjs';
+import { AuthActions } from '../../auth';
 import { CommonUiTestingModule } from '../../common-ui-testing.module';
 
 import { EmailVerificationComponent } from './email-verification.component';
@@ -12,9 +15,7 @@ describe('EmailVerificationComponent', () => {
       imports: [CommonUiTestingModule],
       declarations: [EmailVerificationComponent],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(EmailVerificationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,5 +23,43 @@ describe('EmailVerificationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(`resendSeconds$`, () => {
+    it(`should count down the time in seconds`, async () => {
+      const retryEnables = new Date();
+
+      retryEnables.setSeconds(retryEnables.getSeconds() + 2);
+
+      (component as any).store.dispatch(AuthActions.resendEmailVerificationSuccess({ retryEnables }));
+
+      for (let i = 2; i >= 0; i--) {
+        expect(await firstValueFrom(component.resendSeconds$)).toBe(i);
+
+        await delay(1001);
+      }
+    });
+  });
+
+  describe(`resend`, () => {
+    it(`should dispatch resendEmailVerification`, () => {
+      const dispatchSpy = jest.spyOn((component as any).store, 'dispatch');
+
+      component.resend();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(AuthActions.resendEmailVerification());
+    });
+  });
+
+  describe(`dispatch`, () => {
+    it(`should dispatch verifyEmail with the value of the code field`, () => {
+      const dispatchSpy = jest.spyOn((component as any).store, 'dispatch');
+
+      component.code.setValue('test');
+
+      (component as any).dispatch();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(AuthActions.verifyEmail({ code: 'test' }));
+    });
   });
 });
