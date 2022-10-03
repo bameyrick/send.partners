7ea8d4d345f8ab9807e0d9be30025a265a2209d4
Ben Meyrick <bameyrick@gmail.com>
2022-10-03 10:51:55 +0100

    Improve dropdown tests

--- libs/common-ui/src/lib/components/dropdown/dropdown.component.spec.ts
+++ libs/common-ui/src/lib/components/dropdown/dropdown.component.spec.ts
@@ -1,26 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
 import { CommonUiTestingModule } from '../../common-ui-testing.module';
 
 import { DropdownComponent } from './dropdown.component';
 
+@Component({
+  selector: 'test-component',
+  template: `<dropdown [formControl]="control"><dropdown-option></dropdown-option><dropdown-option></dropdown-option></dropdown>`,
+})
+class TestComponent {
+  @ViewChild(DropdownComponent) public readonly componentRef!: DropdownComponent;
+
+  public readonly control = new FormControl();
+}
+
 describe('DropdownComponent', () => {
   let component: DropdownComponent;
-  let fixture: ComponentFixture<DropdownComponent>;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CommonUiTestingModule],
-      declarations: [DropdownComponent],
+      declarations: [TestComponent, DropdownComponent],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DropdownComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
+    component = fixture.componentInstance.componentRef;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.options?.length).toBe(2);
   });
 });
