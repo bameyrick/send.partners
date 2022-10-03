9885441b6b1257ec443fe66306334d96550a856b
Ben Meyrick <bameyrick@gmail.com>
2022-10-03 14:37:51 +0100

    Login component tests

--- libs/common-ui/src/lib/forms/login/login.component.spec.ts
+++ libs/common-ui/src/lib/forms/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthActions } from '../../auth';
 import { CommonUiTestingModule } from '../../common-ui-testing.module';
 
 import { LoginComponent } from './login.component';
@@ -22,5 +23,18 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(`dispatch`, () => {
+    it(`should dispatch the login action`, () => {
+      const dispatchSpy = jest.spyOn((component as any).store, 'dispatch');
+      const credentials = { email: 'email', password: 'password' };
+
+      component.form.setValue(credentials);
+
+      (component as any).dispatch();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(AuthActions.login({ credentials }));
+    });
   });
 });
