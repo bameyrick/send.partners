320e4d638abe8ac58a13eb4101f454bf35b0adbd
Ben Meyrick <bameyrick@gmail.com>
2022-10-03 13:00:44 +0100

    Add panel tests

--- libs/common-ui/src/lib/components/panel/panel.component.spec.ts
+++ libs/common-ui/src/lib/components/panel/panel.component.spec.ts
@@ -12,9 +12,7 @@ describe('PanelComponent', () => {
       imports: [CommonUiTestingModule],
       declarations: [PanelComponent],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PanelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,5 +20,40 @@ describe('PanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(`ngOnChanges`, () => {
+    describe(`when title is provided`, () => {
+      beforeEach(() => {
+        component.title = 'Test';
+        component.ngOnChanges();
+      });
+
+      it(`should set labeledBy`, () => {
+        expect(component.labeledBy).toBe(`${component.id}-title`);
+      });
+
+      it(`should set titleHTML`, () => {
+        expect(component.titleHTML).toStrictEqual(
+          (component as any).domSanitizer.bypassSecurityTrustHtml(
+            `<h2 id="${(component as any).titleId}" class="${component.bemBlockClass}__title" tabindex="-1">Test</h2>`
+          )
+        );
+      });
+    });
+
+    describe(`when title is not provided`, () => {
+      beforeEach(() => {
+        component.ngOnChanges();
+      });
+
+      it(`should set labeledBy`, () => {
+        expect(component.labeledBy).toBeUndefined();
+      });
+
+      it(`should set titleHTML`, () => {
+        expect(component.titleHTML).toBeUndefined();
+      });
+    });
   });
 });
