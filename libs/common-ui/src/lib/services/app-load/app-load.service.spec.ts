ebc741cbd83a947c99931174cc47cfea2b113e6c
Ben Meyrick <bameyrick@gmail.com>
2022-10-03 16:00:21 +0100

    App load service tests

--- libs/common-ui/src/lib/services/app-load/app-load.service.spec.ts
+++ libs/common-ui/src/lib/services/app-load/app-load.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { delay } from '@qntm-code/utils';
+import { AuthActions } from '../../auth';
 import { CommonUiTestingModule } from '../../common-ui-testing.module';
 
 import { CommonAppLoadService } from './app-load.service';
@@ -16,5 +18,31 @@ describe('CommonAppLoadService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe(`initializeApp`, () => {
+    it(`should dispatch the refreshToken action`, async () => {
+      (service as any).store.dispatch(AuthActions.refreshTokenFailed());
+
+      const dispatchSpy = jest.spyOn((service as any).store, 'dispatch');
+
+      await service.initializeApp();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(AuthActions.refreshToken());
+    });
+
+    it(`should wait for the initial refresh to complete`, async () => {
+      let promiseResolved = false;
+
+      service.initializeApp().then(() => (promiseResolved = true));
+
+      expect(promiseResolved).toBe(false);
+
+      (service as any).store.dispatch(AuthActions.refreshTokenFailed());
+
+      await delay();
+
+      expect(promiseResolved).toBe(true);
+    });
   });
 });
