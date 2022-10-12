567873846e565507ecb7f4df3cee67eec649452c
Ben Meyrick <bameyrick@gmail.com>
2022-10-12 19:23:12 +0100

    Signup name form tests

--- apps/app/src/app/pages/signup/signup-name/signup-name-form/signup-name-form.component.spec.ts
+++ apps/app/src/app/pages/signup/signup-name/signup-name-form/signup-name-form.component.spec.ts
@@ -1,26 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppTestingModule } from '../../../../app-testing.module';
+import { SignupActions } from '../../store';
 
 import { SignupNameFormComponent } from './signup-name-form.component';
 
 describe('SignupNameFormComponent', () => {
   let component: SignupNameFormComponent;
   let fixture: ComponentFixture<SignupNameFormComponent>;
+  let mockStore: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppTestingModule],
       declarations: [SignupNameFormComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
+    mockStore = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(SignupNameFormComponent);
     component = fixture.componentInstance;
+
+    jest.spyOn(mockStore, 'dispatch');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(`dispatch`, () => {
+    it(`should dispath the signup action`, () => {
+      component.form.patchValue({
+        name: `name`,
+      });
+
+      (component as any).dispatch();
+
+      expect(mockStore.dispatch).toHaveBeenCalledWith(
+        SignupActions.setName({
+          name: `name`,
+        })
+      );
+    });
   });
 });
